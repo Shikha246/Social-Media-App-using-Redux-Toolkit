a6bbfed36609752a309c10dba80102aceb2ece28
Shikha <ramrakhyani.shikha@gmail.com>
2026-06-27 16:14:38 +0530

    feat:second commit

--- src/features/posts/Posts.jsx
+++ src/features/posts/Posts.jsx
@@ -1,6 +1,9 @@
-import { useSelector } from "react-redux";
+import { useSelector,useDispatch } from "react-redux";
+import { likeButtonPressed } from "./postsSlice";
+
 
 const Posts = () =>{
+    const dispatch = useDispatch();
     const posts= useSelector((state) => {
         console.log(state.posts);
         return state.posts;
@@ -10,7 +13,7 @@ const Posts = () =>{
             {posts.posts.map((post) =>(
                     <div key = {post.postId}>
                         <p>{post.caption}</p>
-                        <p>Likes:{post.likes}</p>
+                        <button onClick={() => dispatch(likeButtonPressed(post.postId)) }>{post.likes} likes</button>
                     </div>
             ))
             }
